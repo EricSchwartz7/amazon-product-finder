34fdc1aae220b78d7c43b9711cb910eabebf3f7f
Eric Schwartz <ericschwartz7@gmail.com>
2017-04-28 14:14:43 -0400

    temporarily adding config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define (DB_SERVERNAME, "127.0.0.1");
+define (DB_USERNAME, "root");
+define (DB_PASSWORD, "guitar677");
+define (DB_DATABASE, "products");
+
+define (AWS_ACCESS_KEY_ID, "AKIAIOWFZ4KTTJAKNLFQ");
+define (AWS_SECRET_KEY, "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");
+define (ASSOCIATE_ID, "q0d9b-20");
+?>
