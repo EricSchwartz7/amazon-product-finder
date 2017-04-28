602ba17d3ef43232f547bc5d34474d946180f258
Eric Schwartz <ericschwartz7@gmail.com>
2017-04-28 14:25:30 -0400

    trying a different db url

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // define (DB_SERVERNAME, "127.0.0.1");
-define (DB_SERVERNAME, "https://amazon-product-finder.herokuapp.com/");
+define (DB_SERVERNAME, "mysql://b53d882b9b5b2f:a1344c08@us-cdbr-iron-east-03.cleardb.net/heroku_33215d444c36940?reconnect=true");
 define (DB_USERNAME, "root");
 define (DB_PASSWORD, "guitar677");
 define (DB_DATABASE, "products");
