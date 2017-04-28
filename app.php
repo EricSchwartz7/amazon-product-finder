5af8999e101cf4788610d01cdfdd7fe4f11c609d
Eric Schwartz <ericschwartz7@gmail.com>
2017-04-28 11:31:29 -0400

    fix apostrophe bug

--- app.php
+++ app.php
@@ -14,7 +14,7 @@ $uri = "/onca/xml";
 // get the keywords parameter from URL
 if ($_REQUEST["q"]){
   $keywords = $_REQUEST["q"];
-}
+};
 
 $params = array(
     "Service" => "AWSECommerceService",
@@ -29,7 +29,7 @@ $params = array(
 // Set current timestamp if not set
 if (!isset($params["Timestamp"])) {
     $params["Timestamp"] = gmdate('Y-m-d\TH:i:s\Z');
-}
+};
 
 // Sort the parameters by key
 ksort($params);
@@ -38,7 +38,7 @@ $pairs = array();
 
 foreach ($params as $key => $value) {
     array_push($pairs, rawurlencode($key)."=".rawurlencode($value));
-}
+};
 
 // Generate the canonical query
 $canonical_query_string = join("&", $pairs);
@@ -59,27 +59,32 @@ function printSearchResults($parsed_xml){
   $numOfItems = $parsed_xml->Items->TotalResults;
   // print("<table>");
   if($numOfItems>0){
+    $index = 0;
     foreach($parsed_xml->Items->Item as $current){
+      print("<div id=".$index." class='item'>");
       if (isset($current->ASIN)) {
         $asin = $current->ASIN;
-        print("<br>ASIN: ".$asin);
+        print("<br>ASIN: <span class='asin'>".$asin."</span>");
       };
       if (isset($current->ItemAttributes->Title)) {
         $title = $current->ItemAttributes->Title;
-        print("<br>Title: ".$title);
+        print("<br>Title: <span class='title'>".$title."</span>");
       };
       if (isset($current->ItemAttributes->MPN)) {
         $mpn = $current->ItemAttributes->MPN;
-        print("<br>MPN: ".$mpn);
+        print("<br>MPN: <span class='mpn'>".$mpn."</span>");
       };
       if (isset($current->Offers->Offer->OfferListing->Price->FormattedPrice)){
         $formattedPrice = $current->Offers->Offer->OfferListing->Price->FormattedPrice;
         $price = $current->Offers->Offer->OfferListing->Price->Amount;
-        print("<br>Price: ".$formattedPrice);
+        print("<br>Price: <span class='price'>".$formattedPrice."</span>");
       };
       if (isset($current->ASIN)) {
-        print("<br><button type='button' onclick='addToDB(\"".$asin."\", \"".$title."\", \"".$mpn."\", ".$price.")'>Add to DB</button><br>");
+        // print("<br><button type='button' onclick='addToDB(\"".$asin."\", \"".$title."\", \"".$mpn."\", ".$price.")'>Add to DB</button><br>");
+        print("<br><button type='button' onclick='addToDB(".$index.")'>Add to DB</button>");
       };
+      print("</div>");
+      $index++;
     };
   };
 }
@@ -109,7 +114,7 @@ function printData(){
     while($row = $result->fetch_assoc()) {
       // echo "<tr><td>".$row["asin"]."</td></tr>";
       echo "<tr><td>".$row["asin"]
-      ."</td><td>".$row["title"]
+      ."</td><td>".stripslashes($row["title"])
       ."</td><td>".$row["mpn"]
       ."</td><td>$".number_format($row["price"]/100, 2)
       ."</td></tr>";
@@ -121,11 +126,11 @@ function printData(){
   $conn->close();
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+function addToDB(){
 
   // Define the product attributes coming in
   $asin = $_POST["asin"];
-  $title = $_POST["title"];
+  $title = addslashes($_POST["title"]);
   $mpn = $_POST["mpn"];
   $price = $_POST["price"];
 
@@ -151,6 +156,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // print("<p>Added ".$title."</p>");
 
   printData();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  addToDB();
 } elseif (!$_REQUEST["q"]) {
   printData();
 }
