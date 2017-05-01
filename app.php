be5bd592d15b726ec11f7a45aa163646407c013c
Eric Schwartz <ericschwartz7@gmail.com>
2017-05-01 16:29:11 -0400

    add config file

--- app.php
+++ app.php
@@ -101,16 +101,16 @@ function printSearchResults($parsed_xml){
 function printData(){
 
   // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-  //
-  // $server = $url["host"];
-  // $username = $url["user"];
-  // $password = $url["pass"];
-  // $db = substr($url["path"], 1);
-  //
-  // $conn = new mysqli($server, $username, $password, $db);
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $conn = new mysqli($server, $username, $password, $db);
 
   // Create connection
-  $conn = mysqli_connect(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+  // $conn = mysqli_connect(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
   // Check connection
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
